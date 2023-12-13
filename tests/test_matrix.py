ed0bcbe41ca33dce89922d2d4aea7538d61ca134
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2023-12-13 12:03:43 -0800

    tested matrix initialization

---
+++ tests/test_matrix.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.dirname(__file__))
+
+mymodule_dir = os.path.join( script_dir, 'linalg' )
+sys.path.append( mymodule_dir )
+# print(sys.path)
+
+import matrix as linalg
+
+# should print error, invalid input
+# linalg.matrix(1)
+
+# should print error saying 'matrix must have 2 dimensions'
+# linalg.matrix([1, 3, 5]) # 1 dim
+# linalg.matrix([]) # 1 dim
+# linalg.matrix([[[1, 2, 3],[2, 3, 4]]]) # 3 dim
+
+# should throw error about 'invalid matrix'
+'''
+linalg.matrix([
+    [1, 2, 3],
+    [1, 2],
+    [1]
+])
+'''
+
+# should work
+linalg.matrix([
+    [1, 2, 3],
+    [2, 5, 6]
+])
